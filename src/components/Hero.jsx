38a2a0b41e3253fcf8805916e76a8ccbc63881e1
AleixsKenAlvarez <alexisken1432@gmail.com>
2023-02-18 14:35:48 +0800

    added buttons

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
         <>
             <Parallax speed={-50}>
                 <section className='w-full h-screen bg-hero relative text-white overflow-hidden'>
-                    <motion.img initial={{ y: 200}} animate={{ y: 0 }} transition={{ duration: 1 }} alt="HeroRobot" src="/hero.webp" className='absolute bottom-0 lg:right-0 h-full z-0 right-[-15rem] lg:opacity-100 opacity-30 brightness-50 lg:brightness-100  min-w-[45rem]'></motion.img>
+                    <motion.img initial={{ y: 200 }} animate={{ y: 0 }} transition={{ duration: 1 }} alt="HeroRobot" src="/hero.webp" className='absolute bottom-0 lg:right-0 h-full z-0 right-[-15rem] lg:opacity-100 opacity-30 brightness-50 lg:brightness-100  min-w-[45rem]'></motion.img>
 
                     <div className='w-full max-w-[1700px] mx-auto flex items-center h-full lg:px-20 z-10 relative text-center lg:text-left justify-center lg:justify-start'>
                         <div>
@@ -18,17 +18,24 @@ const Hero = () => {
                                 <span className='text-gorange'>R</span>
                                 <span className='text-ggreen'>D </span>
 
-                                
-                                 AI</motion.h1>
+
+                                AI</motion.h1>
                             <motion.div initial={{ x: -200, opacity: 0 }} animate={{ x: 0, opacity: 100 }} transition={{ duration: 1, delay: 0.05 }} className='flex lg:text-3xl text-xl font-bold font-saira gap-x-[7px] mx-auto w-fit lg:mx-0 lg:w-auto min-w-[22rem] items-center justify-center lg:justify-start'>
                                 <h2 className=''>For high quality solutions</h2>
                             </motion.div>
 
-                            <motion.p initial={{ x: -200, opacity: 0 }} animate={{ x: 0, opacity: 100 }} transition={{ duration: 1, delay: 0.1 }} className='mt-7 xl:max-w-[40rem] lg:max-w-[30rem] max-w-[40rem] font-poppins lg:text-md text-sm px-10 lg:px-0 2xl:text-lg'>As you are likely aware, Google has recently announced the forthcoming launch of their ChatGPT artificial intelligence competitor, dubbed "Bard." This initiative, built on the Ethereum Blockchain, leverages Google's cutting-edge AI technology to create compelling AI bots for use on popular platforms such as Telegram and Discord. It is anticipated that this will become the largest AI token on the Ethereum network, reflecting the vibrant and innovative nature of AI technology. We believe that our efforts in the cryptocurrency space will be recognized by Google as a valuable contribution to their ongoing work in this field.</motion.p>
+                            <motion.p initial={{ x: -200, opacity: 0 }} animate={{ x: 0, opacity: 100 }} transition={{ duration: 1, delay: 0.1 }} className='mt-7 xl:max-w-[40rem] lg:max-w-[30rem] max-w-[40rem] font-poppins lg:text-md sm:text-sm text-xs px-10 lg:px-0 2xl:text-lg'>As you are likely aware, Google has recently announced the forthcoming launch of their ChatGPT artificial intelligence competitor, dubbed "Bard." This initiative, built on the Ethereum Blockchain, leverages Google's cutting-edge AI technology to create compelling AI bots for use on popular platforms such as Telegram and Discord. It is anticipated that this will become the largest AI token on the Ethereum network, reflecting the vibrant and innovative nature of AI technology. We believe that our efforts in the cryptocurrency space will be recognized by Google as a valuable contribution to their ongoing work in this field.</motion.p>
 
-                            <motion.button initial={{ x: -200, opacity: 0 }} animate={{ x: 0, opacity: 100 }} transition={{ duration: 1, delay: 0.15 }} className='bg-button px-12 py-3 w-fit h-fit rounded-lg mt-10 hover:bg-[#040B9E] cursor-pointer z-10 relative transition-colors ease-in-out duration-300 mx-auto lg:mx-0'>
-                                <p>Get started</p>
-                            </motion.button>
+                            <motion.div initial={{ x: -200, opacity: 0 }} animate={{ x: 0, opacity: 100 }} transition={{ duration: 1, delay: 0.15 }} className='flex gap-x-3 md:flex-row flex-col items-center mt-10 gap-y-4'>
+                                <button className='bg-button px-12 py-3 w-fit h-fit rounded-lg hover:bg-[#040B9E] cursor-pointer z-10 relative transition-colors ease-in-out duration-300 mx-auto lg:mx-0 font-poppins'>
+                                    <p>Buy Token</p>
+                                </button>
+
+                                <button className='bg-[#A5ADBC] px-12 py-3 w-fit h-fit rounded-lg hover:bg-white hover:text-black font-medium cursor-pointer z-10 relative transition-colors ease-in-out duration-300 mx-auto lg:mx-0 font-poppins text-hero'>
+                                    <p>Charts</p>
+                                </button>
+                            </motion.div>
+
                         </div>
                     </div>
                 </section>
